7127b69f0cfebbaf4080c51a57932582eb8698b4
LazaroMPerez <lazpz61@gmail.com>
2021-06-08 20:07:20 -0400

    Home Component and Home styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, route, Link } from "react-router-dom";
 
 function Home() {
     return (
         <div>
-            
+        
+            <div className="home_header"> 
+                <div className="home_headerLeft">
+                    <Link ></Link>
+                    <Link></Link>
+                    <Link></Link>
+                    <Link></Link>
+                    <Icon/>
+                    <Avatar/>
+                </div>
+
+                <div className="home_headerRight">
+
+                </div>
+            </div>
+
+            <div className="home_body">
+
+            </div>
+
         </div>
     )
 }
